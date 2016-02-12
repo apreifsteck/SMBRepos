3e5674341e563d16f77790bfd94b622f96b91102
Hunter Gulley <hgulley1@live.com>
2016-02-11 21:06:52 -0500

    Added in a database .mdf file

--- WCCFNew/WCCFNew/obj/Debug/WCCFNew_Content.g.i.cs
+++ WCCFNew/WCCFNew/obj/Debug/WCCFNew_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/emaillogo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("createtables.sql")]
 
 
